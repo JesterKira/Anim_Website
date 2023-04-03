ff06ab34e2b0caf020b4151ccfc20e4b98b5d840
JesterKira <jesterbloods@gmail.com>
2023-04-03 09:03:30 +0200

    fix some bugs

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -1,4 +1,4 @@
-const headerNavigation = document.querySelector(".header_navigation-menu");
+const burgeNavigation = document.querySelector(".burger_navigation");
 const hamburgerMenu = document.querySelector(".menu_hamburger");
 const body = document.querySelector("body");
 const navLinks = document.querySelectorAll(".navigation_menu-link")
@@ -6,10 +6,10 @@ const navLinks = document.querySelectorAll(".navigation_menu-link")
 hamburgerMenu.addEventListener("click", function () {
     hamburgerMenu.classList.toggle("active");
     if (hamburgerMenu.classList.contains("active")) {
-        headerNavigation.classList.add("active");
+        burgeNavigation.classList.add("active");
         body.classList.add("lock");
     } else {
-        headerNavigation.classList.remove("active");
+        burgeNavigation.classList.remove("active");
         body.classList.remove("lock");
     }
 })
@@ -18,7 +18,7 @@ for (let navLink of navLinks) {
     navLink.addEventListener("click", function () {
         if (hamburgerMenu.classList.contains("active")) {
             hamburgerMenu.classList.remove("active");
-            headerNavigation.classList.remove("active");
+            burgeNavigation.classList.remove("active");
             body.classList.remove("lock");
         }
     })
